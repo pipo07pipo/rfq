f521034c443db295c378e3bc845587a3e447ceb7
unknown <a5720110a@gmail.com>
2020-07-04 22:33:53 +0700

    renew the database

--- rfq/settings.py
+++ rfq/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'rfq.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rfqdb',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
