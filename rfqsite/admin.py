3854600ab37bf3c18aaa4e2f9f4dfdc43c200c3f
pingmpz <pingmpz@hotmail.com>
2020-07-05 17:20:54 +0700

    dsadsasasd

--- rfqsite/admin.py
+++ rfqsite/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import RFQ, SP_Rate, Active_Rate, Part_Header, SPS, Forcast, Material, Part_Costing, MSUT, CTPP
 # Register your models here.
+admin.site.register(RFQ)
+admin.site.register(SP_Rate)
+admin.site.register(Active_Rate)
+admin.site.register(Part_Header)
+admin.site.register(SPS)
+admin.site.register(Forcast)
+admin.site.register(Material)
+admin.site.register(Part_Costing)
+admin.site.register(MSUT)
+admin.site.register(CTPP)
